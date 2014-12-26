cf236dba50f7eb368051472df57b7cd25f59a73f
Michael Fogleman <michaelwfogleman@gmail.com>
2014-12-26 10:47:59 -0500

    A solution.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,61 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+;; Utils
 
-(defn decode [keyword message]
-  "decodeme")
+(defn bimap
+  [a b]
+  (into (zipmap a b)
+        (zipmap b a)))
+
+(defn rotate
+  [string]
+  (let [f (str (first string))
+        r (clojure.string/join (rest string))
+        result (str r f)]
+    (lazy-seq
+     (cons
+      result
+      (rotate result)))))
+
+;; Variables
+
+(def alphabet "abcdefghijklmnopqrstuvwxyz")
+
+(def chart (cons alphabet (take 25 (rotate alphabet))))
+
+(def lookup-map
+  (bimap (map str alphabet) (iterate inc 0)))
+
+;; Functions
+
+(defn encoding
+  [a b]
+  (let [row (lookup-map a)
+        column (lookup-map b)]
+    (-> chart
+        (nth row)
+        (nth column)
+        str)))
+
+(defn decoding
+  [row letter]
+  (let [num (lookup-map row)]
+    (-> chart
+        (nth num)
+        (.indexOf letter)
+        lookup-map
+        str)))
+
+(defn coding
+  [f]
+  (fn [keyword input]
+    (->> input
+         (interleave (cycle keyword))
+         (map str)
+         (partition 2)
+         (mapcat #(apply f %))
+         (apply str))))
+
+(def encode (coding encoding))
+
+(def decode (coding decoding))
--- alphabet-cipher/test/alphabet_cipher/coder_test.clj
+++ alphabet-cipher/test/alphabet_cipher/coder_test.clj
@@ -1,17 +1,31 @@
 (ns alphabet-cipher.coder-test
   (:require [clojure.test :refer :all]
-            [alphabet-cipher.coder :refer :all]))
+            [alphabet-cipher.coder :refer :all])
+  (:use midje.sweet))
 
-(deftest test-encode
-  (testing "can encode give a secret keyword"
-    (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
-           (encode "vigilance" "meetmeontuesdayeveningatseven")))
-    (is (= "egsgqwtahuiljgs"
-           (encode "scones" "meetmebythetree")))))
+(facts "about rotate and the resulting chart"
+       (fact "rotate works"
+             (->> (rotate "abc") (take 1) first) => "bca")
+       (tabular
+        (fact "and we have a chart"
+              (?fn chart) => ?result)
+        ?fn ?result
+        count 26
+        first alphabet
+        last "zabcdefghijklmnopqrstuvwxy"))
 
-(deftest test-decode
-  (testing "can decode an cyrpted message given a secret keyword"
-    (is (= "meetmeontuesdayeveningatseven"
-           (decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv")))
-    (is (= "meetmebythetree"
-           (decode "scones" "egsgqwtahuiljgs")))))
+(facts "about lookup and numeric-lookup"
+       (fact "numeric-lookup takes a numeric column and a row"
+             (numeric-lookup 18 12) => "e")
+       (fact "lookup works with letters"
+             (lookup "s" "m") => "e"))
+
+(facts "about encode"
+       (fact "it can encode give a secret keyword"
+             (encode "vigilance" "meetmeontuesdayeveningatseven") => "hmkbxebpxpmyllyrxiiqtoltfgzzv"
+             (encode "scones" "meetmebythetree") => "egsgqwtahuiljgs"))
+
+(facts "about decode"
+       (fact "it can decode an encrypted message given a secret keyword"
+             (decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv") => "meetmeontuesdayeveningatseven"
+             (decode "scones" "egsgqwtahuiljgs") => "meetmebythetree"))
